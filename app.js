5c9f3b86f6cef2092028e81989cdf448e6451668
Natalie Ung <ung.natalie@gmail.com>
2017-11-10 17:00:46 -0500

    completed section 2

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-// file here
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var fs = require('fs');
+var path = require('path');
+var bodyParser = require('body-parser');
+var nunjucks = require('nunjucks');
+var env = nunjucks.configure('views', {noCache: true});
+
+
+
+
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+
+
+app.use(express.static(__dirname + '/public'))
+
+// app.get('/', function (req, res, next) {
+// 	res.
+// })
+
+
+app.listen(3000, function () {
+	console.log('listening on port 3000');
+});
