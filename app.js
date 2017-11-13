0a10c363b5e7613f0b6d5bb8d504440a098bb08c
Claudia Baik <claudiabaik@gmail.com>
2017-11-13 12:00:32 -0500

    upto Add a page

--- app.js
+++ app.js
@@ -6,23 +6,37 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var nunjucks = require('nunjucks');
 var env = nunjucks.configure('views', {noCache: true});
-var models = require('./models/index.js')               //connects to models-index.js
+var models = require('./models/index.js')          //connects to models-index.js
+var routes = require('./routes'); //by default ot will go to index.js
 
 
 
 
-
-app.set('view engine', 'html');
+app.set('view engine', 'html'); //(name, value)
 app.engine('html', nunjucks.render);
 
 
 app.use(express.static(__dirname + '/public'))
-
+app.use(function (req, res, next) {
+	console.log('req.method & path: ', req.method, req.path);
+	next();
+})
+app.use('/', routes); //actually using methods in routes
 // app.get('/', function (req, res, next) {
 // 	res.
 // })
 
 
-app.listen(3000, function () {
-	console.log('listening on port 3000') ;
-});
+// app.listen(3000, function () {
+// 	console.log('listening on port 3000') ;
+// });
+
+// make sure you are exporting your db from your models file
+models.db.sync({force: true})
+.then(function () {
+    // make sure to replace the name below with your express app
+    app.listen(3000, function () {
+        console.log('Server is listening on port 3000!');
+    });
+})
+.catch(console.error);
--- models/index.js
+++ models/index.js
@@ -1,33 +1,53 @@
 var Sequelize = require('sequelize');
-var db = new Sequelize('postgres://localhost:5432/wikistack');
+var db = new Sequelize('postgres://localhost:5432/wikistack', {
+    logging: false
+});
 console.log('it worked')
 
 var Page = db.define('page', {
   title: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING,
+      allowNull: false
   },
   urlTitle: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING,
+      allowNull: false,
+    //   get() {
+    //       var wikilink = this.getDataValue()
+    //   }
   },
   content: {
-      type: Sequelize.TEXT
+      type: Sequelize.TEXT,
+      allowNull: false
   },
   status: {
       type: Sequelize.ENUM('open', 'closed')
+  },
+  date: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW
   }
 });
 
 var User = db.define('user', {
   name: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING,
+      allowNull: false
   },
   email: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true
+        }
   }
 });
 
 module.exports = {
 Page: Page,
-User: User
+User: User,
+db: db    //connecting  db to app.js when you wanna use to sync in app.js
 };
 
+//want a dfined server route for each page
+//a model instance proprty that we might want to call route
